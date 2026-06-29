f2e07e31da157893f4822e093b00f5fd78b23ae2
Mrkingoff <hebatanakhebat@gmail.com>
2026-06-29 13:53:11 +0700

    new

---
+++ settings/config.js
@@ -0,0 +1,9 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6282228893045']
+global.namaowner = '༆ 𝙕𝙔𝙔 ☬'
+
+//----ALL PAYMENT----//
+global.dana = "081997350433"
+global.gopay = "081997350433"
+global.ovo = false
+global.qris = false
